efc35cbdbaf807d7d2876d12721f3c83b25e6ab7
wilsonstevensCapy <stevenscard06@gmail.com>
2021-10-22 00:29:48 -0500

    mercado liebre casi listo

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express();
 const path = require("path");
 const port = process.env.PORT || 3000;
+
 app.use(express.static(path.resolve(__dirname, "./public")));
+
 app.get("/", (req, res) => {
-    let htmlPath = path.resolve(__dirname,"./views/home.html");
-    res.sendFile(htmlPath);
-  });
+  let htmlPath = path.resolve(__dirname, "./views/home.html");
+  res.sendFile(htmlPath);
+});
+app.get("/register", (req, res) => {
+  let htmlPath = path.resolve(__dirname, "./views/register.html");
+  res.sendFile(htmlPath);
+});
+app.get("/login", (req, res) => {
+  let htmlPath = path.resolve(__dirname,"./views/login.html");
+  res.sendFile(htmlPath);
+});
 app.listen(port, () => {
   console.log(`servidor corriendo en el puerto ${port}`);
- });
+});
